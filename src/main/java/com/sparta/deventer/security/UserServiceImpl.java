dfc16db4bcd9d5b74744e59e144a7238635ca9c5
Cho Koung Min <andy123423@naver.com>
2024-06-20 15:30:48 +0900

    feat: sign up & login 기능 구현

--- src/main/java/com/sparta/deventer/security/UserServiceImpl.java
+++ src/main/java/com/sparta/deventer/security/UserServiceImpl.java
@@ -1,8 +1,8 @@
 package com.sparta.deventer.security;
 
+import com.sparta.deventer.entity.User;
 import com.sparta.deventer.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -14,8 +14,11 @@ public class UserServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return (UserDetails) userRepository.findByUsername(username)
+    public UserDetailsImpl loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new IllegalArgumentException("해당하는 유저가 없습니다."));
+
+        return new UserDetailsImpl(user);
+
     }
 }
