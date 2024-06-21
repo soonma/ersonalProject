3033cc8e36e665cef655c2d17a8434a1ea6e5b98
soonma <clsrn77990@naver.com>
2024-06-21 15:18:46 +0900

    feet : 게시글과 댓글 같이 조회가능으로 변경
    게시글과 댓글 동시에 나오는걸로 변경했습니다, Comment 작성시간 수정시간도 반환하도록 수정했습니다.

--- src/main/java/com/sparta/deventer/dto/CommentResponseDto.java
+++ src/main/java/com/sparta/deventer/dto/CommentResponseDto.java
@@ -4,6 +4,7 @@ package com.sparta.deventer.dto;
 import com.sparta.deventer.entity.Comment;
 import com.sparta.deventer.entity.Post;
 import com.sparta.deventer.entity.User;
+import java.time.LocalDateTime;
 import lombok.Getter;
 
 @Getter
@@ -11,12 +12,17 @@ public class CommentResponseDto {
     private String content;
     private Long id;
     private User user;
-    private Post post;
+//    private Post post;
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
+
 
     public CommentResponseDto(Comment comment) {
         this.content =comment.getContent();
         this.id = comment.getId();
         this.user = comment.getUser();
-        this.post = comment.getPost();
+//        this.post = comment.getPost();
+        this.createAt = comment.getCreatedAt();
+        this.updateAt = comment.getUpdateAt();
     }
 }
--- src/main/java/com/sparta/deventer/dto/PostResponseDto.java
+++ src/main/java/com/sparta/deventer/dto/PostResponseDto.java
@@ -1,5 +1,22 @@
 package com.sparta.deventer.dto;
 
+import com.sparta.deventer.entity.Post;
+import lombok.Getter;
+
+
+@Getter
 public class PostResponseDto {
+    private Long postid;
+    private String title;
+    private String content;
+    private String categoryTopic;
+
+
+    public PostResponseDto(Post post) {
+        this.postid = post.getId();
+        this.title = post.getTitle();
+        this.content = post.getContent();
+        this.categoryTopic = post.getCategory().getTopic();
+    }
 
 }
