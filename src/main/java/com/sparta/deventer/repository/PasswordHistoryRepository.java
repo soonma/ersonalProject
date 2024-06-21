bc4921cc6230815be35c1962f119316a497e3bbf
sillysillyman <sillysillyman.cs@gmail.com>
2024-06-21 16:27:54 +0900

    feat: PasswordHistoryRepository에 findByUserOrderByCreatedAtAsc 메서드 추가
    - User 엔티티를 기반으로 PasswordHistory를 생성일 순서대로 조회하는 메서드 추가

--- src/main/java/com/sparta/deventer/repository/PasswordHistoryRepository.java
+++ src/main/java/com/sparta/deventer/repository/PasswordHistoryRepository.java
@@ -1,8 +1,11 @@
 package com.sparta.deventer.repository;
 
 import com.sparta.deventer.entity.PasswordHistory;
+import com.sparta.deventer.entity.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PasswordHistoryRepository extends JpaRepository<PasswordHistory, Long> {
 
+    List<PasswordHistory> findByUserOrderByCreatedAtAsc(User user);
 }
