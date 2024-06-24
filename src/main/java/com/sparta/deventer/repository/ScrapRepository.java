c97d52ce74079663548170b9cef808056c9519d7
sillysillyman <sillysillyman.cs@gmail.com>
2024-06-24 16:47:32 +0900

    feat: ScrapRepository에 findAllByUser 메서드 추가
    - 특정 사용자의 모든 스크랩을 조회하기 위한 findAllByUser 메서드를 ScrapRepository에 추가

--- src/main/java/com/sparta/deventer/repository/ScrapRepository.java
+++ src/main/java/com/sparta/deventer/repository/ScrapRepository.java
@@ -3,6 +3,7 @@ package com.sparta.deventer.repository;
 import com.sparta.deventer.entity.Post;
 import com.sparta.deventer.entity.Scrap;
 import com.sparta.deventer.entity.User;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -10,4 +11,5 @@ public interface ScrapRepository extends JpaRepository<Scrap, Long> {
 
     Optional<Scrap> findByUserAndPost(User user, Post post);
 
+    List<Scrap> findAllByUser(User user);
 }
